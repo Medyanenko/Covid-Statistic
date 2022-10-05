481b63878a371453566dc4d7e9bac5a70456cd2d
Medyanenko <t.kudryavska@gmail.com>
2022-10-05 16:06:56 +0200

    fix margin-top bug

--- src/components/GlobalStatistic/GlobalStatistic.jsx
+++ src/components/GlobalStatistic/GlobalStatistic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GlobalStatistic = () => {
   return (
     <div>
-      <h2>juhyiu</h2>
+      juhyiu
     </div>
   )
 }
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import s from './Sidebar.module.css';
+import s from "./Sidebar.module.css";
 
 const Sidebar = (props) => {
   return (
     <nav className={s.nav}>
       <div className={s.item}>
-        <Link className={s.link} to="/globalStatistic">Global</Link>
+        <Link className={s.link} to="/globalStatistic">
+          Global
+        </Link>
       </div>
       <div className={s.item}>
-        <Link className={s.link} to="/countriesStatistic">Countries</Link>
+        <Link className={s.link} to="/countriesStatistic">
+          Countries
+        </Link>
       </div>
       <div className={s.item}>
-        <Link className={s.link} to="/about">About</Link>
+        <Link className={s.link} to="/about">
+          About
+        </Link>
       </div>
     </nav>
   );
